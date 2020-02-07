f731d7b816e9596636dd0acd2c2d93610498ef03
John Souza <souzajohnlee@gmail.com>
2020-02-07 18:47:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,16 @@
 # Code your solution here!
+def random_number
+  rand(1..6)
+end
+
+def ask_for_input
+  puts "Please enter a number from 1 to 6"
+end
+
+def player_input
+  gets.chomp
+end
+
+def correct_guess?
+  player_guess == correct_number
+end
