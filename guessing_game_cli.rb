1364d24984744a4382454484f8eaa2d988e782b4
John Souza <souzajohnlee@gmail.com>
2020-02-07 18:55:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -22,7 +22,7 @@ def run_guessing_game
   player_guess = player_input
   if player_guess == "exit"
     puts "Goodbye!"
-  elsif player_guess.to_i == correct_number
+  elsif correct_guess?(player_guess.to_i, correct_number)
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{correct_number}."
